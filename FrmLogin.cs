c210c92e31fc3945f7404577923ed47f0a24ca83
153321276@qq.com <153321276@qq.com>
2020-07-09 15:14:45 +0800

    优化登陆模块,封装连接数据库类SqlHelper

--- FrmLogin.cs
+++ FrmLogin.cs
@@ -39,22 +39,16 @@ namespace WinStudent
 
             //与数据库通讯,账号密码比对
             {
-                //建立与数据库的连接
-                //连接数据库的字符串
-                //string connString = "server=.;database=StudentDB;Intergrated Security=true;";//windows身份验证
-                string connString = "server=.;database=StudentDB;uid=sa;pwd=sa123;";//SQL验证方式
-                SqlConnection conn = new SqlConnection(connString);
+                
                 //写查询语句
-                string sql = "select count(1) from UserInfos where UserName='" + uName + "' and UserPwd='" + uPwd + "'";
-                //创建command对象
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                //cmd.CommandType = CommandType.StoredProcedure;//如果执行存储过程,需要加上这句
-                //打开连接
-                conn.Open();//最晚打开,最早关闭
-                //执行命令,要求必须在连接状态下
-                object o = cmd.ExecuteScalar();//执行查询,返回结果集第一行第一列的值,忽略其他行或者列
-                //关闭连接
-                conn.Close();
+                string sql = "select count(1) from UserInfos where UserName=@UserName and UserPwd=@UserPwd";
+                
+                SqlParameter[] paras =
+                {
+                    new SqlParameter("@UserName", uName),
+                    new SqlParameter("@UserPwd", uPwd)
+                };
+                object o = SqlHelper.ExecuteScalar(sql, paras);
                 //处理结果
                 //根据返回的结果,进行不同的提示和页面跳转
                 if (o==null||o==DBNull.Value || ((int)o) == 0)
---
+++ SqlHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WinStudent
+{
+    public class SqlHelper
+    {
+        //获取服务器地址和登陆账号密码
+        public static readonly string connString = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+        public static object ExecuteScalar(string sql,params SqlParameter[] paras)
+        {
+            //string connString = "server=.;database=StudentDB;Intergrated Security=true;";//windows身份验证
+            object o = null;  
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                //创建command对象
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //cmd.CommandType = CommandType.StoredProcedure;//如果执行存储过程,需要加上这句
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddRange(paras);
+                //打开连接
+                conn.Open();//最晚打开,最早关闭
+                //执行命令,要求必须在连接状态下
+                o = cmd.ExecuteScalar();//执行查询,返回结果集第一行第一列的值,忽略其他行或者列
+            } 
+            return o;
+        }
+    }
+}
