ea1d529e5c53db14cf5371b30b02c337092f185c
xiaozn <153321276@qq.com>
2020-07-09 08:05:18 +0800

    哦哦哦

--- FrmLogin.Designer.cs
+++ FrmLogin.Designer.cs
@@ -38,18 +38,20 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(110, 217);
+            this.btnLogin.Location = new System.Drawing.Point(82, 174);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.Size = new System.Drawing.Size(56, 25);
             this.btnLogin.TabIndex = 0;
             this.btnLogin.Text = "登陆";
             this.btnLogin.UseVisualStyleBackColor = true;
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(237, 217);
+            this.btnExit.Location = new System.Drawing.Point(178, 174);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(56, 25);
             this.btnExit.TabIndex = 1;
             this.btnExit.Text = "退出";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -57,40 +59,44 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(62, 63);
+            this.label1.Location = new System.Drawing.Point(46, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 2;
             this.label1.Text = "账号:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(62, 128);
+            this.label2.Location = new System.Drawing.Point(46, 102);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.Size = new System.Drawing.Size(35, 12);
             this.label2.TabIndex = 3;
             this.label2.Text = "密码:";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(165, 63);
+            this.txtUserName.Location = new System.Drawing.Point(124, 50);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(176, 25);
+            this.txtUserName.Size = new System.Drawing.Size(133, 21);
             this.txtUserName.TabIndex = 4;
             // 
             // txtUserPwd
             // 
-            this.txtUserPwd.Location = new System.Drawing.Point(165, 125);
+            this.txtUserPwd.Location = new System.Drawing.Point(124, 100);
+            this.txtUserPwd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUserPwd.Name = "txtUserPwd";
-            this.txtUserPwd.Size = new System.Drawing.Size(176, 25);
+            this.txtUserPwd.Size = new System.Drawing.Size(133, 21);
             this.txtUserPwd.TabIndex = 5;
             // 
             // FrmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 319);
+            this.ClientSize = new System.Drawing.Size(318, 255);
             this.Controls.Add(this.txtUserPwd);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label2);
@@ -98,6 +104,7 @@
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnLogin);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "FrmLogin";
             this.Text = "登陆页面";
