9c17d4b56cfb76ae15789b9688a3ac8a1e8c9bb3
xiaozn <153321276@qq.com>
2020-07-16 22:01:13 +0800

    班级列表的功能实现

--- FrmClassList.Designer.cs
+++ FrmClassList.Designer.cs
@@ -28,19 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboGrades = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClassName = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.dgvClassList = new System.Windows.Forms.DataGridView();
+            this.ClassId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ClassName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GradeName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Remark = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClassList)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnFind);
+            this.groupBox1.Controls.Add(this.txtClassName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cboGrades);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(442, 73);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "查询条件";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "年级";
+            // 
+            // cboGrades
+            // 
+            this.cboGrades.FormattingEnabled = true;
+            this.cboGrades.Location = new System.Drawing.Point(59, 33);
+            this.cboGrades.Name = "cboGrades";
+            this.cboGrades.Size = new System.Drawing.Size(121, 20);
+            this.cboGrades.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "班级名称";
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.Location = new System.Drawing.Point(247, 33);
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(100, 21);
+            this.txtClassName.TabIndex = 3;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(353, 33);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 4;
+            this.btnFind.Text = "查询";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // dgvClassList
+            // 
+            this.dgvClassList.AllowUserToAddRows = false;
+            this.dgvClassList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvClassList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClassList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ClassId,
+            this.ClassName,
+            this.GradeName,
+            this.Remark});
+            this.dgvClassList.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvClassList.Location = new System.Drawing.Point(0, 101);
+            this.dgvClassList.Name = "dgvClassList";
+            this.dgvClassList.RowTemplate.Height = 23;
+            this.dgvClassList.Size = new System.Drawing.Size(466, 240);
+            this.dgvClassList.TabIndex = 1;
+            // 
+            // ClassId
+            // 
+            this.ClassId.DataPropertyName = "ClassId";
+            this.ClassId.HeaderText = "编号";
+            this.ClassId.Name = "ClassId";
+            // 
+            // ClassName
+            // 
+            this.ClassName.DataPropertyName = "ClassName";
+            this.ClassName.HeaderText = "班级名称";
+            this.ClassName.Name = "ClassName";
+            this.ClassName.ReadOnly = true;
+            // 
+            // GradeName
+            // 
+            this.GradeName.DataPropertyName = "GradeName";
+            this.GradeName.HeaderText = "年级名称";
+            this.GradeName.Name = "GradeName";
+            this.GradeName.ReadOnly = true;
+            // 
+            // Remark
+            // 
+            this.Remark.DataPropertyName = "Remark";
+            this.Remark.HeaderText = "班级描述";
+            this.Remark.Name = "Remark";
+            this.Remark.ReadOnly = true;
             // 
             // FrmClassList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(466, 341);
+            this.Controls.Add(this.dgvClassList);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FrmClassList";
             this.Text = "班级列表";
+            this.Load += new System.EventHandler(this.FrmClassList_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClassList)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtClassName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboGrades;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvClassList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClassId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClassName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GradeName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Remark;
     }
 }
--- FrmClassList.cs
+++ FrmClassList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,71 @@ namespace WinStudent
         public FrmClassList()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// 初始化年级列表，所有班级列表信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrmClassList_Load(object sender, EventArgs e)
+        {
+            InitGrades();
+            InitAllClasses();
+        }
+        /// <summary>
+        /// 加载所有班级信息
+        /// </summary>
+        private void InitAllClasses()
+        {
+            string sql= "select ClassId,ClassName,GradeName,Remark from ClassInfo c inner join GradeInfo g on c.GradeId = g.GradeId";
+            DataTable dtClasses = SqlHelper.GetDataTable(sql);
+            dgvClassList.DataSource = dtClasses;
+        }
+
+        /// <summary>
+        /// 加载年级列表
+        /// </summary>
+        private void InitGrades()
+        {
+            string sql = "select GradeId,GradeName from GradeInfo";
+            DataTable dtGradeList = SqlHelper.GetDataTable(sql);
+            //添加一个  ---请选择项
+            DataRow dr = dtGradeList.NewRow();
+            dr["GradeId"] = 0;
+            dr["GradeName"] = "请选择";
+            dtGradeList.Rows.InsertAt(dr,0);
+            cboGrades.DataSource = dtGradeList;
+            cboGrades.DisplayMember = "GradeName";
+            cboGrades.ValueMember = "GradeId";
+            cboGrades.SelectedIndex = 0;
+        }
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            //获取查询条件
+            int gradeId =(int) cboGrades.SelectedValue;
+            string className = txtClassName.Text.Trim();
+            string sql = "select ClassId,ClassName,GradeName,Remark from ClassInfo c inner join GradeInfo g on c.GradeId=g.GradeId";
+            sql += " where 1=1";
+            if (gradeId > 0)
+            {
+                sql += " and c.GradeId=@GradeId";
+            }
+            if (!string.IsNullOrEmpty(className))
+            {
+                sql += " and ClassName like @ClassName";
+            }
+            SqlParameter[] paras =
+            {
+                new SqlParameter("@GradeId",gradeId),
+                new SqlParameter("@ClassName","%"+className+"%")
+            };
+            DataTable dtClasses = SqlHelper.GetDataTable(sql,paras);
+            dgvClassList.DataSource = dtClasses;
         }
     }
 }
