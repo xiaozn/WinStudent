84155d5cf7bb93cc9b80b302540a8264fc4c7c83
xiaozn <153321276@qq.com>
2020-07-18 17:52:26 +0800

    新增学生界面和功能实现

--- FrmAddStudent.Designer.cs
+++ FrmAddStudent.Designer.cs
@@ -28,19 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtStuName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboClasses = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rbtMale = new System.Windows.Forms.RadioButton();
+            this.rbtFemale = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtStuName
+            // 
+            this.txtStuName.Location = new System.Drawing.Point(110, 49);
+            this.txtStuName.Name = "txtStuName";
+            this.txtStuName.Size = new System.Drawing.Size(121, 21);
+            this.txtStuName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "姓名:";
+            // 
+            // cboClasses
+            // 
+            this.cboClasses.FormattingEnabled = true;
+            this.cboClasses.Location = new System.Drawing.Point(110, 91);
+            this.cboClasses.Name = "cboClasses";
+            this.cboClasses.Size = new System.Drawing.Size(121, 20);
+            this.cboClasses.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "班级:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "性别:";
+            // 
+            // rbtMale
+            // 
+            this.rbtMale.AutoSize = true;
+            this.rbtMale.Location = new System.Drawing.Point(110, 133);
+            this.rbtMale.Name = "rbtMale";
+            this.rbtMale.Size = new System.Drawing.Size(35, 16);
+            this.rbtMale.TabIndex = 5;
+            this.rbtMale.TabStop = true;
+            this.rbtMale.Text = "男";
+            this.rbtMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtFemale
+            // 
+            this.rbtFemale.AutoSize = true;
+            this.rbtFemale.Location = new System.Drawing.Point(182, 133);
+            this.rbtFemale.Name = "rbtFemale";
+            this.rbtFemale.Size = new System.Drawing.Size(35, 16);
+            this.rbtFemale.TabIndex = 6;
+            this.rbtFemale.TabStop = true;
+            this.rbtFemale.Text = "女";
+            this.rbtFemale.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "电话:";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(110, 171);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(121, 21);
+            this.txtPhone.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(64, 233);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(175, 233);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // FrmAddStudent
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 334);
+            this.ClientSize = new System.Drawing.Size(311, 303);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.rbtFemale);
+            this.Controls.Add(this.rbtMale);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboClasses);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStuName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
             this.Name = "FrmAddStudent";
             this.Text = "新增学生";
+            this.Load += new System.EventHandler(this.FrmAddStudent_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtStuName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboClasses;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rbtMale;
+        private System.Windows.Forms.RadioButton rbtFemale;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClose;
     }
 }
--- FrmAddStudent.cs
+++ FrmAddStudent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,102 @@ namespace WinStudent
         public FrmAddStudent()
         {
             InitializeComponent();
+        }
+
+        private void FrmAddStudent_Load(object sender, EventArgs e)
+        {
+            //加载班级列表，性别默认男
+            InitClasses();
+            rbtMale.Checked = true;
+        }
+
+        private void InitClasses()
+        {
+            string sql = "select ClassId,ClassName,GradeName from ClassInfo c ,GradeInfo g where c.GradeId=g.GradeId";
+            DataTable dtClasses = SqlHelper.GetDataTable(sql);
+            //班级年级组合项添加
+            if (dtClasses.Rows.Count > 0)
+            {
+                foreach (DataRow dr in dtClasses.Rows)
+                {
+                    string className = dr["ClassName"].ToString();
+                    string gradeName = dr["GradeName"].ToString();
+                    dr["ClassName"] = className + " -- " + gradeName;
+                }
+            }
+            
+            cboClasses.DataSource = dtClasses;
+            cboClasses.DisplayMember = "ClassName";
+            cboClasses.ValueMember = "ClassId";
+            cboClasses.SelectedIndex = 0;
+        }
+        /// <summary>
+        /// 添加学生信息功能
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //获取页面信息输入
+            string stuName = txtStuName.Text.Trim();
+            int classId = (int)cboClasses.SelectedValue;
+            string sex = rbtMale.Checked ? rbtMale.Text.Trim() : rbtFemale.Text.Trim();
+            string phone = txtPhone.Text.Trim();
+            //判断是否为空
+            if (string.IsNullOrEmpty(stuName))
+            {
+                MessageBox.Show("姓名不能为空", "添加学生", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(phone))
+            {
+                MessageBox.Show("电话不能为空", "添加学生", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //判断姓名+电话是否再数据库中存在
+            string sql = "select count(1) from StudentInfo where StudentName=@StudentName and Phone=@Phone";
+            SqlParameter[] paras =
+            {
+                new SqlParameter(@"StudentName",stuName),
+                new SqlParameter(@"Phone",phone)
+            };
+            object o = SqlHelper.ExecuteScalar(sql, paras);
+            if(o==null || o==DBNull.Value || ((int)o) == 0)
+            {
+                //插入操作
+                string sqlAdd = "insert into StudentInfo (StudentName,ClassId,Sex,Phone) values (@StudentName,@ClassId,@Sex,@Phone)";
+                SqlParameter[] paraAdd =
+                {
+                    new SqlParameter("@StudentName",stuName),
+                    new SqlParameter("@ClassId",classId),
+                    new SqlParameter("@Sex",sex),
+                    new SqlParameter("@Phone",phone)
+                };
+                int count = SqlHelper.ExecuteNoQuery(sqlAdd, paraAdd);
+                if (count > 0)
+                {
+                    MessageBox.Show($"学生:{stuName} 添加成功", "添加学生", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("该学生添加失败，请假查！", "添加学生", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("该学生已存在！", "添加学生", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
